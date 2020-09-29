d8bef26eb79f05283224bea863e85217ef89f275
Lysander <lysander.hans@hotmail.com>
2020-09-29 15:53:49 +0200

    created class componen (rcc+tab/enter)

---
+++ src/components/Person.jsx
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Person extends Component {
+    render() {
+        return (
+            <div>
+                Jean-Jâcques
+            </div>
+        );
+    }
+}
+
+export default Person;
