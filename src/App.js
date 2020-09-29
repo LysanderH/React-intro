acb1955ebd37d1372c8a176fe622239a2df3016c
Lysander <lysander.hans@hotmail.com>
2020-09-29 16:59:56 +0200

    created counter class

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import Person from "./components/Person";
+import Counter from "./components/Counter";
 
 function App() {
+    const increment = () => {
+
+    }
+
     return (
         <div className="App">
             <header className="App-header">
-                <Person age={25} name="Lysander" loc="Liège"/>
+                {/*<Person age={25} name="Lysander" loc="Liège"/>*/}
+                <Counter />
             </header>
         </div>
     );
---
+++ src/components/Counter.jsx
@@ -0,0 +1,33 @@
+import React, {Component} from 'react';
+
+class Counter extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            count: 0,
+        }
+    }
+
+    addOne = () => {
+        this.setState({count: this.state.count + 1});
+    }
+
+    substractOne = () => {
+        this.setState({count: this.state.count > 0 ? this.state.count - 1 : this.state.count});
+    }
+
+    render() {
+        return (
+            <section>
+                <h2 aria-level="2" role="heading">Compteur basique</h2>
+                <p>
+                    <span>{this.state.count}</span>
+                    <button onClick={this.addOne}>+</button>
+                    <button onClick={this.substractOne}>+</button>
+                </p>
+            </section>
+        );
+    }
+}
+
+export default Counter;
